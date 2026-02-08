bd5e6f14019f93ee8ea2ad8f9d66b3c4ee042494
Vijay Kant Singh <155866301+VijayChaudhary2557@users.noreply.github.com>
2026-02-08 22:00:12 +0530

    listion commit

--- server.js
+++ server.js
@@ -74,7 +74,7 @@ app.use((err, req, res, next) => {
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server running on port http://localhost:${PORT}`);
-});
+// app.listen(PORT, () => {
+//   console.log(`Server running on port http://localhost:${PORT}`);
+// });
 
